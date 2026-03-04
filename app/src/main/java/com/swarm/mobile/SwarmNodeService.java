7d025d3563ca9c346399711b1c086bfee2511b62
Nandor Komlodi <nandor.komlodi@gmail.com>
2026-03-04 17:19:41 +0100

    fix: unused import

--- app/src/main/java/com/swarm/mobile/SwarmNodeService.java
+++ app/src/main/java/com/swarm/mobile/SwarmNodeService.java
@@ -1,6 +1,5 @@
 package com.swarm.mobile;
 
-import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
